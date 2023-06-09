06621d14c88dd6112a94a5af3e8b4d8fd6053cd4
Tien-PM24 <115676176+Tien-PM24@users.noreply.github.com>
2023-06-09 18:01:27 +0700

    add function add,change and delete

--- app/Http/Controllers/API_LazadaController.php
+++ app/Http/Controllers/API_LazadaController.php
@@ -1,15 +1,35 @@
 <?php
 
-namespace App\Http\Controllers;
-
 use App\Models\t_lazada;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\File;
 
-class API_LazadaController extends Controller
+class LazadaController extends Controller
 {
-    public function getItems() {
+    public function getItems()
+    {
         $items = t_lazada::all();
         return response()->json($items);
+    }
+
+    public function createItem(Request $request)
+    {
+        $data = $request->all();
+        $item = t_lazada::create($data);
+        return response()->json($item);
+    }
+
+    public function updateItem(Request $request, $id)
+    {
+        $data = $request->all();
+        $item = t_lazada::findOrFail($id);
+        $item->update($data);
+        return response()->json($item);
+    }
+
+    public function deleteItem($id)
+    {
+        $item = t_lazada::findOrFail($id);
+        $item->delete();
+        return response()->json(['message' => 'Item deleted successfully']);
     }
 }
